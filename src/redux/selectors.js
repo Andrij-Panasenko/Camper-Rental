6a7f05be1a614baae00bbdabe937371649373f0c
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-04 20:39:01 +0300

    add filter selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,3 +3,7 @@ export const selectCampers = state => state.catalog.items
 export const selectFavoriteCampers = state => state.catalog.favoriteList;
 
 export const selectIsLoading = state => state.catalog.isLoading;
+
+export const selectVehicleEquipFilters = state => state.filters.vehicleEquipment;
+
+export const selectVehicleTypeFilters = state => state.filters.vehicleType;
